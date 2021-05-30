909f8acd5a96550c91f4236e5ff079c6b7ba1c0d
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-30 12:38:16 +0530

    [Rohit]ADD: Create a Stack of 56>30>70

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedListStack = new LinkedList();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
